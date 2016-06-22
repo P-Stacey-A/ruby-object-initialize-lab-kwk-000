c07fc983b9f951be8d56b4f17d434a4c71421b64
Pamela Alvarez <p.stacey.a@gmail.com>
2016-06-22 23:20:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+
+
+
+    class Dog
+
+      def initialize(name, breed = "Mut")
+          @name = name
+          @breed = breed
+      end
+
+
+      def breed=(name, breed)
+        @name = name
+        @breed = breed
+      end
+
+      def breed (name, breed)
+         @name
+         @breed
+      end
+
+    end
